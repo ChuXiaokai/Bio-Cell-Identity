e5ccdabcca231d78c513d8ff158843d0c572023c
ChuXiaokai <chuxiaokai@ict.ac.cn>
2019-04-04 21:31:34 +0800

    add CESA3

---
+++ run.bat
@@ -0,0 +1,6 @@
+python main.py "3283-CESA3-1/3283-CESA3 1/" "CESA3 3-3283" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 5/" "3283-CESA3 5-1" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 9/" "3283-cesa3 9" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 14/" "3283-CESA3 14" 0.717 png
+python main.py "3283-CESA3-1/3283-CESA3 21/" "3283-CESA3 21-1" 0.717 png
+python main.py "3283-CESA3-1/3283-CESA3 2/" "cesa3-3283 4" 0.65 png
--- run.sh
+++ run.sh
@@ -1,16 +1,6 @@
-python main.py "CESA3_1/CESA3-gcs-pre-mature/CESA3-pre-mature-4/4-white/" "GFP-CESA3 4" 170 png
-python main.py "CESA3_1/CESA3-gcs-pre-mature/CESA3-pre-mature-11/11-white/" "GFP-CESA3 11" 200 png
-python main.py "CESA3_1/CESA3-gcs-pre-mature/CESA3-pre-mature-36/white-36/" "GFP-CESA3 36" 180 png
-python main.py "CESA3_1/CESA3-gcs-pre-mature/CESA3-pre-mature-52/52-white/" "GFP-CESA3 52" 240 png
-
-
-python main.py "CESA3_1/CESA3-GCs-mature/CESA3-9-mature/9-white/" "white-9" 210 png
-python main.py "CESA3_1/CESA3-GCs-mature/CESA3-13-mature/13-white/" "13-white" 200 png
-python main.py "CESA3_1/CESA3-GCs-mature/CESA3-37-mature/37-white/" "GFP-CESA3 37" 220 png
-python main.py "CESA3_1/CESA3-GCs-mature/CESA3-68-mature/68-white/" "GFP-CESA3 68" 200 png
-
-# tif
-python main.py "CESA3_1/CESA3-GCs-mature/CESA3-12-mature/12-white/" "GFP-CESA3 12" 200 tif
-python main.py "CESA3_1/CESA3-GCs-mature/CESA3-27-mature/white-27/" "GFP-CESA3 27" 235 tif
-
-python main.py "3283-CESA3-1/3283-CESA3 1/" "CESA3 3-3283" 180 png
+python main.py "3283-CESA3-1/3283-CESA3 1/" "CESA3 3-3283" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 5/" "3283-CESA3 5-1" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 9/" "3283-CESA3 12" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 14/" "3283-CESA3 14" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 21/" "3283-CESA3 21-1" 0.7 png
+python main.py "3283-CESA3-1/3283-CESA3 2/" "cesa3-3283 4" 0.7 png
